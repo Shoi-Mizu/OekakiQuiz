2d64c21d96c19e7b5d8e672e0e68a3bf737ebefe
shoi <takuan0113@gmail.com>
2021-02-07 20:42:45 +0900

    お題のオブジェクトを生成する

---
+++ Assets/Scripts/GameScene/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviourPunCallbacks
+{
+    private GameObject odai;
+    public static int odaiCount = 1;
+
+
+    //ゲームがスタートしたら、お題のオブジェクトを生成する
+    void Update()
+    {
+        if(MasterManager.startOn == true){
+            if(odaiCount == 1){
+            odai = (GameObject)PhotonNetwork.Instantiate("OdaiObject", Vector2.zero, Quaternion.identity);
+            odaiCount--;                
+            }
+        }
+    }
+}
