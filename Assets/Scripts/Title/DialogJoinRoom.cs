e690759447fd87c95b51d62832586b09987dda4d
shoi <takuan0113@gmail.com>
2021-02-07 13:00:05 +0900

    ルームに参加するダイアログの処理

---
+++ Assets/Scripts/Title/DialogJoinRoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogJoinRoom : MonoBehaviour
+{
+    public AudioClip buttonOn;
+    public AudioClip canselOn;
+    private AudioSource audioSource;
+    private InputField input;
+ 
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>(); //オーディオソースの取得
+        input = this.transform.Find("DialogPanel").gameObject.transform.Find("InputField").gameObject.GetComponent<InputField>(); //入力部のコンポーネントを取得
+    }
+
+    //ルームに参加する
+    public void Join(){
+        //入力が空白じゃない
+        if((input.text.Equals(null) == false) && (input.text.Equals("") == false)){
+            audioSource.PlayOneShot(buttonOn);
+            TitleManager.roomName = input.text; //ルーム名をTitleManagerに渡す
+            TitleManager.joinOn = true; //ルームに参加
+            TitleManager.startOn = true;
+            Destroy(this.gameObject, 1f);
+        }
+    }
+
+    //キャンセル
+    public void Cancel(){
+        audioSource.PlayOneShot(canselOn);
+        Destroy(this.gameObject, 0.2f);
+    }
+}
