8a2ae9c1b98e991c664af5dd8745c03257f7e457
shoi <takuan0113@gmail.com>
2021-02-07 21:13:56 +0900

    お題を見るボタンを押した時の処理

---
+++ Assets/Scripts/GameScene/OdaiLookButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class OdaiLookButton : MonoBehaviourPunCallbacks
+{
+    public GameObject odaiCanvas;
+    private GameObject odaiDialog;
+    public AudioClip buttonOn;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    public void OdaiLook(){
+        //描き手か
+        if(photonView.IsMine){
+            audioSource.PlayOneShot(buttonOn);
+            odaiDialog = (GameObject)Instantiate(odaiCanvas);
+        }
+    }
+}
