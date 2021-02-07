734374e5458b056466c65c7029d4c8a8d2514639
shoi <takuan0113@gmail.com>
2021-02-07 12:47:46 +0900

    ローディングの画像を消す

---
+++ Assets/Scripts/Title/Loading.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Loading : MonoBehaviourPunCallbacks
+{
+
+    //ルーム作成が失敗した時、オブジェクト削除
+    public override void OnCreateRoomFailed(short returnCode, string message){
+        Destroy(this.gameObject);
+    }
+
+    //マッチングが失敗した時、オブジェクト削除
+    public override void OnJoinRoomFailed(short returnCode, string message){
+        Destroy(this.gameObject);
+    }
+}
