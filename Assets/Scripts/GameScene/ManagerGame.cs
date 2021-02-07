cfab4736dd494ed0ec69c165d2ad2e68f72fcaf0
shoi <takuan0113@gmail.com>
2021-02-07 21:27:15 +0900

    ルーム入室直後の処理

---
+++ Assets/Scripts/GameScene/ManagerGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ManagerGame : MonoBehaviourPunCallbacks
+{
+     void Start()
+    {
+        PhotonNetwork.IsMessageQueueRunning = true; //入室時に受信を許可する
+        PhotonNetwork.Instantiate("ClearObject", Vector2.zero, Quaternion.identity); //描画クリアオブジェクトの生成
+        PhotonNetwork.Instantiate("UserIcon", Vector2.zero, Quaternion.identity); //プレイヤーのアイコン生成
+        PhotonNetwork.InstantiateSceneObject("CanvasTimer", Vector2.zero, Quaternion.identity); //タイマー表示部の生成
+        PhotonNetwork.Instantiate("FlagManager", Vector2.zero, Quaternion.identity); //フラグ管理オブジェクトの生成
+    }
+}
