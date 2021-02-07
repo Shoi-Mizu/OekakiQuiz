2487a52940be4d0eb7fbd8807f7aa2ee0b2a74ed
shoi <takuan0113@gmail.com>
2021-02-07 12:26:44 +0900

    アプリのバージョンを表示

---
+++ Assets/Scripts/Title/VersionTextScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class VersionTextScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var version = Application.version; 
+        GetComponent<Text>().text = "Ver : " + version;
+    }
+}
