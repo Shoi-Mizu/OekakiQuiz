d4c54b240c9d02f4e8d931f1273d6f28c87e82b2
shoi <takuan0113@gmail.com>
2021-02-07 20:08:32 +0900

    通信失敗時のダイアログを閉じる処理

---
+++ Assets/Scripts/Title/CancelButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelButton : MonoBehaviour
+{
+    public AudioClip canselOn;
+    private AudioSource audioSource;
+
+    void Start(){
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    //閉じるボタン押した時の処理
+    public void Cancel(){
+        audioSource.PlayOneShot(canselOn);
+        Destroy(this.gameObject, 0.2f);
+    }
+}
