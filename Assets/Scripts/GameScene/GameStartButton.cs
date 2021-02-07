3b7fd8a44d25dfca5876b384b171d1d9df1334b7
shoi <takuan0113@gmail.com>
2021-02-07 21:28:49 +0900

    スタートボタンを押した時の処理

---
+++ Assets/Scripts/GameScene/GameStartButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartButton : MonoBehaviour
+{
+    
+    public static bool gameStartOn = false;
+    public GameObject canvasDialog;
+    private GameObject dialog;
+    public AudioClip buttonOn;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    //スタートボタンを押した時の処理
+    public void GameStart(){
+        if((gameStartOn == false) && (MasterManager.startOn == false) &&(ContinueCanvas.continueOn == false)){
+            audioSource.PlayOneShot(buttonOn);
+            dialog = (GameObject)Instantiate(canvasDialog);
+        }
+    }
+
+}
