796d7663373383d9ce680e47b33010f3e9ba6a7b
shoi <takuan0113@gmail.com>
2021-02-08 17:12:11 +0900

    退出のダイアログの処理

---
+++ Assets/Scripts/GameScene/DialogExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogExit : MonoBehaviour
+{
+    public AudioClip buttonOn;
+    public AudioClip canselOn;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    //退出するを押した時
+    public void GameExit(){
+        audioSource.PlayOneShot(buttonOn);
+        ExitButton.exit = true;
+        Destroy(this.gameObject);
+    }
+
+    //キャンセルした時
+    public void Cancel(){
+        audioSource.PlayOneShot(canselOn);
+        Destroy(this.gameObject, 0.2f);
+    }
+}
