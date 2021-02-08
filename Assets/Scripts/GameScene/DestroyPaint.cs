9242a9d793d404615193115adcad6086a53791dc
shoi <takuan0113@gmail.com>
2021-02-08 17:16:36 +0900

    描画したものを消す処理

---
+++ Assets/Scripts/GameScene/DestroyPaint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPaint : MonoBehaviour
+{
+
+    //クリアボタンが押されたら、描画した物を消す
+    void Update()
+    {
+        if(Clear.clearOn == true){
+            Destroy(this.gameObject);
+        }
+    }
+}
