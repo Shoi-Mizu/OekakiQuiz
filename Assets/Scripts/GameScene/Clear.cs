92c9b93adb82c4811288d5641292c25d04b1dc15
shoi <takuan0113@gmail.com>
2021-02-08 17:30:57 +0900

    クリアボタンを押した時の処理

---
+++ Assets/Scripts/GameScene/Clear.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Clear : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public static bool clearOn;
+    private float destroyTime;
+
+    void Start()
+    {
+        destroyTime = 0.1f;
+    }
+
+
+    void Update()
+    {
+        if(ClearButton.clearOn == true){
+            clearOn = ClearButton.clearOn;
+        }
+        if(ClearButton.clearOn == false){
+            if(clearOn == true){
+                destroyTime -= Time.deltaTime; //描画が確実に消えるまでの時間を確保
+                if(destroyTime < 0){
+                    clearOn = false;
+                    destroyTime = 0.13f;
+                }
+            }
+        }
+    }
+
+    //クリアボタンが押されたか同期
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+        if(stream.IsWriting){
+            stream.SendNext(clearOn);
+        } else {
+            clearOn = (bool)stream.ReceiveNext();
+        }
+    }
+}
---
+++ Assets/Scripts/GameScene/ClearButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ClearButton : MonoBehaviourPunCallbacks
+{
+    public static bool clearOn;
+    private float destroyTime; 
+    public AudioClip clearSE;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        clearOn = false;
+        destroyTime = 0.1f;
+    }
+
+
+    void Update()
+    {
+        if(clearOn == true){
+            destroyTime -= Time.deltaTime; //描画が確実に消えるまでの時間を確保
+            if(destroyTime < 0){
+                clearOn = false;
+                destroyTime = 0.13f;
+            }
+        }
+    }
+
+    //クリアボタンを押した時
+    public void Clear(){
+        if(photonView.IsMine){
+            audioSource.PlayOneShot(clearSE);
+            clearOn = true;       
+        }
+    }
+}
