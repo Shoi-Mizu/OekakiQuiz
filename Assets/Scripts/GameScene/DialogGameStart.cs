d29dfbdf58aa32a64bbea974d69201835f410331
shoi <takuan0113@gmail.com>
2021-02-08 17:11:15 +0900

    ゲームスタートのダイアログの処理

---
+++ Assets/Scripts/GameScene/DialogGameStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class DialogGameStart : MonoBehaviour
+{
+    public AudioClip buttonOn;
+    public AudioClip canselOn;
+    private AudioSource audioSource;
+
+    void Start(){
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    //ゲームを始めるボタンを押した時
+    public void GameStart(){
+        audioSource.PlayOneShot(buttonOn);
+        GameStartButton.gameStartOn = true;
+        Destroy(this.gameObject, 0.2f);
+    }
+
+    //キャンセルを押した時
+    public void Cancel(){
+        audioSource.PlayOneShot(canselOn);
+        Destroy(this.gameObject, 0.2f);
+    }
+}
