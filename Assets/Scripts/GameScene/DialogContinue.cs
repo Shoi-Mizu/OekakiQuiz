9c96dcb6e7837ff54f5737429fadc08a8007e70c
shoi <takuan0113@gmail.com>
2021-02-08 17:15:03 +0900

    コンティニューのダイアログの処理

---
+++ Assets/Scripts/GameScene/DialogContinue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class DialogContinue : MonoBehaviour
+{
+    public AudioClip buttonSE;
+    private AudioSource audioSource;
+
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    //コンティニューした時
+    public void Continue(){
+        audioSource.PlayOneShot(buttonSE);
+        ContinueCanvas.kaisuu += 1;
+        ContinueCanvas.continueOn = false;
+        ContinueCanvas.destroy = true;
+    }
+
+    //退出した時
+    public void GameExit(){
+        audioSource.PlayOneShot(buttonSE);
+        ContinueCanvas.destroy = true;
+        ExitButton.exit = true;
+    }
+
+}
