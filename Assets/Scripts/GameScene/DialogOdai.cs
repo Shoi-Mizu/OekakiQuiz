0000e23b044bb84568274b5ea07d9ce94a92a8f0
shoi <takuan0113@gmail.com>
2021-02-08 17:10:06 +0900

    お題のダイアログの処理

---
+++ Assets/Scripts/GameScene/DialogOdai.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class DialogOdai : MonoBehaviour
+{
+    public AudioClip buttonOn;
+    public AudioClip canselOn;
+    private AudioSource audioSource;
+    public Text odaiText;
+
+    void Start()
+    {
+        odaiText.text = OdaiData.odai;
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    //閉じるボタンを押した時
+    public void Toji(){
+        audioSource.PlayOneShot(canselOn);
+        Destroy(this.gameObject, 0.2f);
+    }
+
+    //パスした時
+    public void Path(){
+        audioSource.PlayOneShot(buttonOn);
+        if(MasterManager.startOn == true){
+            TimerText.pathOn = true;
+        }
+        Destroy(this.gameObject, 0.2f);
+    }
+}
